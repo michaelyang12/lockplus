051de065a789797491cd20d77b71b80aff6775e1
justinneff5 <59897146+justinneff5@users.noreply.github.com>
2021-10-01 17:19:00 -0500

    landing page mockup 50% front end

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,28 @@ import Image from 'next/image';
 
 export default function Home() {
   return (
-    <div className="h-full w-full bg-gray-100">
-      <div className="text-lg text-black font-md font-sans">HI</div>
+    <div className="h-screen w-screen bg-black">
+      <div className="block h-24 w-full border border-lockplus-blue"></div>
+      <div className="w-2/3 h-96 mx-auto mt-162">
+        <div className="flex justify-center block w-auto h-48 mt-24 m-4 ">
+          <div className="relative h-24 w-64 mt-14">
+            <div className="text-6xl font-bold font-lockplus text-right text-lockplus-blue pr-4">
+              lock+
+            </div>
+            <button className="bg-lockplus-blue h-6 w-24 absolute right-4 rounded-full focus:outline-none transform hover:scale-102 hover:bg-lockplus-hoverblue mt-1">
+              <div className="h-auto w-auto mx-auto text-black font-light text-sm font-lockplus">
+                Order Now
+              </div>
+            </button>
+          </div>
+          <div className="h-42 w-64 text-md font-lockplus text-left text-lockplus-blue tracking-wider font-snug leading-tight pt-16">
+            <div className="w-full h-auto">Affordable home security.</div>
+            <div className="w-full h-auto">Advanced biometrics.</div>
+            <div className="w-full h-auto">Safety, simpler than ever.</div>
+            <div className="w-full h-auto text-white">Learn More.</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        lockplus: {
+          blue: '#0596FF',
+          hoverblue: '#42B0FF',
+        },
+      },
+      fontFamily: {
+        lockplus: ['Oxygen'],
+      },
+      scale: {
+        102: '1.02',
+      },
+    },
   },
   variants: {
     extend: {},
