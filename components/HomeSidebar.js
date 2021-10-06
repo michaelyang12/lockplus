d4e4651d749c8fa8da9e4215c162a259e29fe606
michaelyang12 <michaelyang@wustl.edu>
2021-10-06 17:41:14 -0500

    blahasd

--- components/HomeSidebar.js
+++ components/HomeSidebar.js
@@ -3,21 +3,42 @@ import { useRouter } from 'next/router';
 
 export default function HomeSidebar() {
   const router = useRouter();
+  
+
+
   return (
     <>
       <div class="relative min-h-screen md:flex">
         <div class="bg-gray-800 text-gray-100 flex justify-between md:flex">
-            <div class="sidebar bg-lockplus-blue text-blue-100 w-40 space-y-6 py-7 px-2 absolute inset-y-0 left-0 transform -translate-x-full md:relative md:translate-x-0 transition duration-200 ease-in-out">
-            <a href="#" class="text-white flex items-center space-x-2 px-4">
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <div class="sidebar bg-lockplus-blue text-blue-100 w-40 space-y-6 py-7 px-2 absolute inset-y-0 left-0">
+            <a href="#" class="text-white flex items-center space-x-2 px-4 mb-12">
+              <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z" />
-            </svg>  
-                <span class="text-2xl font-bold font-lockplus">lock +</span>
+              </svg>
+              <span class="text-2xl font-bold font-lockplus">lock +</span>
             </a>
-            </div>
+            <a href="#" class="text-white flex items-center space-x-2 px-4 ml-1 hover:text-lockplus-hoverGray">
+              <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z" />
+              </svg>
+              <span class="text-lg font-bold font-lockplus">users</span>
+            </a>
+            <a href="#" class="text-white flex items-center space-x-2 px-4 ml-1 hover:text-lockplus-hoverGray">
+              <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" />
+              </svg>
+              <span class="text-lg font-bold font-lockplus">history</span>
+            </a>
+            <a href="#" class="text-white flex items-center space-x-2 px-4 ml-1 hover:text-lockplus-hoverGray">
+              <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z" />
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+              </svg>
+              <span class="text-lg font-bold font-lockplus">settings</span>
+            </a>
+          </div>
         </div>
       </div>
-
     </>
   );
 }
---
+++ components/usersPage.js
@@ -0,0 +1,11 @@
+import { useState } from 'react';
+import { useRouter } from 'next/router';
+
+export default function usersPage() {
+    const router = useRouter();
+  return (
+    <div className="container mx-auto">
+                
+    </div>
+  );
+}
