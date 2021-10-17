f83d39aaf1137f424fc5b46fe4b2603f85c619cd
justinneff5 <59897146+justinneff5@users.noreply.github.com>
2021-10-17 16:11:27 -0500

    committing backend setup

---
+++ util/database.js
@@ -0,0 +1,32 @@
+import mongoose from 'mongoose';
+
+const MONGODB_URI = process.env.MONGO_URI;
+
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+async function connectDB() {
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    const opts = {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      bufferCommands: false,
+      useCreateIndex: true,
+    };
+
+    cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
+      return mongoose;
+    });
+  }
+  cached.conn = await cached.promise;
+  return cached.conn;
+}
+
+export default connectDB;
