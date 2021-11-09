4833878337a8cedff7f5ee967304e94e6f3393bb
justinneff5 <59897146+justinneff5@users.noreply.github.com>
2021-11-09 17:41:53 -0600

    comment out

--- components/PhotosForm.tsx
+++ components/PhotosForm.tsx
@@ -48,7 +48,7 @@ export const PhotosForm = (props) => {
     const apiUrl: string = '/api/dbPhotos/' + code + '/' + safeUser;
     const response: any = await axios.post(apiUrl, formData, config);
     setUploadSuccess(response.data.message);
-    console.log('response', response.data);
+    //console.log('response', response.data);
   };
 
   return (
